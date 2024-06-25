aca9895f9c81c40e07bb0ad4f7f8663dc00a2442
Clear008 <wissal-exo@hotmail.com>
2024-06-25 23:46:25 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,8 @@
-function getResponseFromAPI() {
+function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
-        if (true) {
-            resolve('success');
-        } else {
-            reject('error');
-        }
+      if (success) resolve({ status: 200, body: 'Success' });
+      reject(Error('The fake API is not working currently'));
     });
-}
-export default getResponseFromAPI;
+  }
+  
+  export default getFullResponseFromAPI;
